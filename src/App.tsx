9814f70d4b682abc6ab303aaaef088be6b37a421
Miki Namsrai <miki.namsrai@gmail.com>
2022-12-23 22:46:27 +0100

    Create structured files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
-    return (
-        <div className="App">
-            <div className="text-red-300">coucou</div>
-        </div>
-    );
+	return (
+		<div className="App">
+			<div className="text-red-300">coucou</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ src/components/player/Shortcuts.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+function Shortcuts({}: Props) {
+	return <div>Shortcuts</div>
+}
+
+export default Shortcuts
